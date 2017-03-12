bb9b111dc156984327bccdde65ab4aa2373f2704
real name genij <zgxzgx@gmail.com>
2017-03-12 15:41:12 -0500

    Update again

--- gamemodes/microcosm/entities/entities/micro_item_salainen_kookospahkina_puu.lua
+++ gamemodes/microcosm/entities/entities/micro_item_salainen_kookospahkina_puu.lua
@@ -5,68 +5,86 @@
 --micro_item_salainen_kookospahkina_puu copy-paste
 
 --TODO: Make sure SERVER and CLIENT stuff are correct...!
---have a timer that detracts energy based on traits and adds energy based on traits also
 
 AddCSLuaFile()
 
 ENT.Base = "micro_item"
 
-ENT.ItemName = "Puu"
+ENT.ItemName = "Kookospahkina"
 ENT.ItemModel = "models/hunter/misc/sphere025x025.mdl"
 ENT.MaxCount = 1
 
 function ENT:Use(ply)
 	--plant the coconut
-	--[[if !self.isPlanted then --TODO: check material beneathe to make sure it's on land that can support growing a tree. Also check to make sure there is enough horizontal room?
+	if !self.isPlanted && self.treeLevel == 0 && self:WaterLevel() == 0 then --TODO: && self:OnGround() 
 		self.isPlanted = true
-		print("homo fagest")
-		print(self.treeModels)
-		PrintTable(self.treeModels)
+		--stop movement
+		self:SetMoveType(0)
+		print("FROZEN: ")
+		--make sound
+		self:EmitSound("phx/eggcrack.wav")
 		for i = 1,#self.treeModels do
 			timer.Simple( self.growthSpeed, function()
 				self:SetModel(i)
+				print("TIMER RUNNING?!")
 			end )
 			self.treeLevel = i
 		end
-	end]]
+	end
+	print("current growthspeed: " .. self.growthSpeed)
+	print(self.isPlanted)
+	print(self.treeLevel .. " should be 0")
+	print(self:WaterLevel() .. " should be 0")
 end
 
 function ENT:Initialize()
 	self:SetMaterial("models/props_pipes/guttermetal01a")
+	self:SetModel(self.ItemModel)
 	self:PhysicsInitStandard()
 	self.isPlanted = false
 	self.treeLevel = 0
 	self.treeModels = {"models/props_foliage/ferns03.mdl", "models/props_foliage/ferns02.mdl", "models/props/de_dust/palm_tree_head_skybox.mdl", "models/props_foliage/urban_small_palm01.mdl",
 						"models/props_foliage/mall_palmplant02.mdl", "models/props_foliage/mall_palm01_medium.mdl", "models/props_foliage/mall_palm01.mdl"}
 	self.growthSpeed = 5
-	self.numberOfFruit = 1
-	self.energy = 10000
+	self.numberOfFruit = 2
+	self.startingEnergy = 10000
+	self.energy = self.startingEnergy
 
 	if SERVER then
+		--energy timer
 		local timer_name = "treeEnergyDepletion_" .. self:EntIndex()
-		timer.Create(timer_name,120,0, function() --every two minutes, update energy status
-			if IsValid(self) then
-				self.energy = self.energy - (120 / self.growthSpeed) --wew?
-				--[[ fruit production
-				if treeLevel > a certain number then
+		timer.Create(timer_name,100,0, function() --every 100s, update energy status
+			if IsValid(self) && self.isPlanted then
+				self.energy = self.energy - ((1000 * self.growthSpeed) + 0) + 100*self.treelevel --wew?
+				print(self.energy)
+				-- fruit production
+				if treeLevel > 7 && math.rand() < self.growthSpeed then
 					--bear fruit
-					Ent create micro_item_salainen_kookospahkina_puu
+					--Ent create micro_item_salainen_kookospahkina_puu
+					newFruit = ents.Create("micro_item_salainen_kookospahkina_puu")
+					if ( !IsValid( newFruit ) ) then return end
+					for i = 1,self.numberOfFruit do
+						newFruit:SetPos(self:GetPos() + Vector(math.Random(-25,25),math.Random(-25,25),256))
 						--at position up in the tree (might need some if statements to do bearing fruit earlier/later)
 							--maybe use the entity's height subtract some?
 						--set new values for growth speed and gestation or w/e on new entity
-						self.setGenes(1, 2, 3)
+						newFruit:setGenes(self.growthSpeed, self.numberOfFruit, self.startingEnergy)
+						self.energy = self.energy - (self.startingEnergy * self.numberOfFruit)
+						newFruit:Spawn()
+					end
 				end
-				--]]
-				--[[ autoplant functionality
-				if treeLevel == 0 then
-					timer.Simple( self.growthSpeed, function()
-						if math.Rand() > 0.75 then
-							--plant itself. 
-							self.Use()
-						end
-					end )
+				--
+				if self.energy <= 0 then --KILL FUNCTION; SLAYER
+					self:Remove()
 				end
-				--]]
+			elseif IsValid(self) && treeLevel == 0 && math.Rand() > 0.75 then
+				-- autoplant functionality
+				if self.energy <= 0 then --make sure plant not really dead somehow
+					self:Remove()
+				end
+				--plant itself.
+				self.Use()
+				--
 			else
 				timer.Remove(timer_name)
 			end
@@ -75,14 +93,16 @@ function ENT:Initialize()
 end
 
 if SERVER then
-	function ENT:setGenes(setGrowthSpeed, setNumberOfFruit, setEnergy)
-		newGrowthSpeed = setGrowthSpeed + math.Random(-1,1)
-		if newGrowthSpeed < 5 then --5 seconds for debugging. 10 minute days, 600 IRL seconds / day, so 300 is half a day.
-			newGrowthSpeed = 5
-		elseif newGrowthSpeed > 3000 then
-			newGrowthSpeed = 3000 --55 minutes IRL, or 5 days in game at 600 IRL seconds / day
+	function ENT:setGenes(setGrowthSpeed, setNumberOfFruit, setStartingEnergy)
+		newGrowthSpeed = setGrowthSpeed + (math.Random(-2,2)/100) --chance to grow food every 100 seconds
+		if newGrowthSpeed < 0.03 then --1 every 3000s, or 50m
+			newGrowthSpeed = 0.03
+		elseif newGrowthSpeed > 0.34 then --1 every 300s
+			newGrowthSpeed = 0.34
 		end
+		print("old value: " .. self.growthSpeed)
 		self.growthSpeed = newGrowthSpeed
+		print("new value: " .. self.growthSpeed)
 
 		newNumberOfFruit = setNumberOfFruit + math.Random(-1,1)
 		if newNumberOfFruit < 1 then
@@ -93,12 +113,14 @@ if SERVER then
 		self.numberOfFruit = newNumberOfFruit
 
 		--setEnergy here, this is to ensure that the plant lives for some time and also dies eventually.
-		newEnergy = self.energy + math.Random(-1,1)
-		if newGrowthSpeed < 600 then
-			newGrowthSpeed = 600
-		elseif newGrowthSpeed > 1000000 then
-			newGrowthSpeed = 1000000
+		newStartingEnergy = self.startingEnergy + math.Random(-1,1)
+		if newStartingEnergy < 600 then
+			newStartingEnergy = 600
+		elseif newStartingEnergy > 1000000 then
+			newStartingEnergy = 1000000
 		end
+		self.startingEnergy = newStartingEnergy
+		self.energy = self.startingEnergy
 	end
 
 	--if fertilizer is introduced like in SS13, then make a getGenes...
